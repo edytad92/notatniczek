431b5a2c7bd8817a1dfa94ed7f6ea22f7eb268b7
Edyta Dzięglewska <edytadzieglewska@wp.pl>
2019-09-07 10:38:24 +0200

    init

---
+++ src/main/java/pl/sda/notatniczek/NotatniczekApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.notatniczek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotatniczekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotatniczekApplication.class, args);
+    }
+
+}
